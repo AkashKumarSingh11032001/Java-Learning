b1f1ad04b4a8242ad7cf6b2b7e2635a669bac888
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-01-14 21:40:29 +0530

    Programs

---
+++ src/com/akash/Swapping.java
@@ -0,0 +1,17 @@
+package com.akash;
+
+public class Swapping {
+    public static void main(String[] arg){
+        int a,b;
+        a=1;
+        b=2;
+        //write the code to exchange values of a and b here
+        int c;
+        c = a;
+        a = b;
+        b = c;
+        //The following two lines will print values of a and b
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+}
---
+++ src/com/akash/rough.java
@@ -0,0 +1,12 @@
+package com.akash;
+
+public class rough {
+
+    public static void main(String[] args ) {
+
+        double a = 5*5;
+
+        System.out.println(a);
+
+    }
+}
